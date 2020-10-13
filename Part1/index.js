65a9dd0ca3c18fcb1295139c298c4cbd4a4caf7d
Alex Bean <alexbean95@gmail.com>
2020-10-13 14:37:02 -0600

    finished Part 1, but not black diamond

--- Part1/index.js
+++ Part1/index.js
@@ -1,78 +1,75 @@
-console.log("hello")
-
-const board=[]
+const board = []
 
 function play (clickedId) {
-   const  playerSpan = document.getElementById ("player");
-   const clickedElement = document.getElementById(clickedId)
-   if (playerSpan.innerText === "X") {
-       playerSpan.innerText = "O"
-       clickedElement.innerText ="X"
-       board[clickedId] = "X"
-   } else {
+    let playerSpan = document.getElementById("player")
+    let clickedElement = document.getElementById(clickedId)
+
+    if (playerSpan.innerText === "X") {
+        playerSpan.innerText = "O"
+        clickedElement.innerText = "X"
+        board[clickedId] = "X"
+    } else {
         playerSpan.innerText = "X"
         clickedElement.innerText = "O"
         board[clickedId] = "O"
-   }
-   console.log(board)
+    }
+    console.log(board)
 
-  const topLeft = board[0];
-  const topCenter = board[1];
-  const topRight = board[2];
-  const middleLeft = board[3];
-  const middleCenter = board[4];
-  const middleRight = board[5];
-  const bottomLeft = board[6];
-  const bottomCenter = board[7];
-  const bottomRight = board[8];
+    const topLeft = board[0];
+    const topCenter = board[1];
+    const topRight = board[2];
+    const middleLeft = board[3];
+    const middleCenter = board[4];
+    const middleRight = board[5];
+    const bottomLeft = board[6];
+    const bottomCenter = board[7];
+    const bottomRight = board[8];
 
-  if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
-    alert(`${topLeft} is the winner`)
-  }
+    if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
+        alert(`${topLeft} is the winner`)
+    }
   
-  if (topLeft !== undefined && topLeft === middleLeft && topLeft === bottomLeft) {
-    alert(`${topLeft} is the winner`)
-  }
+    if (topLeft !== undefined && topLeft === middleLeft && topLeft === bottomLeft) {
+        alert(`${topLeft} is the winner`)
+    }
 
-  if (topLeft !== undefined && topLeft === middleCenter && topLeft === bottomRight) {
-    alert(`${topLeft} is the winner`)
-  }
+    if (topLeft !== undefined && topLeft === middleCenter && topLeft === bottomRight) {
+        alert(`${topLeft} is the winner`)
+    }
 
-  if (topCenter !== undefined && topCenter === middleCenter && topCenter === bottomCenter) {
-    alert(`${topCenter} is the winner`)
-  }
+    if (topCenter !== undefined && topCenter === middleCenter && topCenter === bottomCenter) {
+        alert(`${topCenter} is the winner`)
+    }
 
-  if (topRight !== undefined && topRight === middleRight && topRight === bottomRight) {
-    alert(`${topRight} is the winner`)
-  }
+    if (topRight !== undefined && topRight === middleRight && topRight === bottomRight) {
+        alert(`${topRight} is the winner`)
+    }
 
-  if (topRight !== undefined && topRight === middleCenter && topRight === bottomLeft) {
-    alert(`${topRight} is the winner`)
-  }
+    if (topRight !== undefined && topRight === middleCenter && topRight === bottomLeft) {
+        alert(`${topRight} is the winner`)
+    }
 
-  if (middleLeft !== undefined && middleLeft === middleCenter && middleLeft === middleRight) {
-    alert(`${middleLeft} is the winner`)
-  }
+    if (middleLeft !== undefined && middleLeft === middleCenter && middleLeft === middleRight) {
+        alert(`${middleLeft} is the winner`)
+    }
 
-  if (bottomLeft !== undefined && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
-    alert(`${bottomLeft} is the winner`)
-  }
+    if (bottomLeft !== undefined && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
+        alert(`${bottomLeft} is the winner`)
+    }
 
-  const boardFull = true
-for (let i = 0; i <= 8; i++) {
-    if (board[i] === undefined) {
-        boardFull = false
-        }
+    let boardFull = true
+    for (let i = 0; i <= 8; i++) {
+        if (board[i] === undefined) {
+            boardFull = false
+        } 
     }
     if (boardFull === true) {
-        alert ("It's a cat's game")
+        alert("It's a cat's game")
     }
-}
 
-function blackDiamond () {
-    for (let i=0; i<= 8; i++) {
-        let reset = document.getElementById(i)
-        reset.innerText = ""
+    function blackDiamond () {
+          if (boardFull === true || alert()) {
+            board[i] = undefined
+      }
     }
-    document.getElementById('player').innerText = 'X'
 }
